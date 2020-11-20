2b12650978806be3dd62a614e3983267f2a3cc58
Axieof <pritheev24@gmail.com>
2020-11-20 19:29:27 +0800

    Added MainMenu

---
+++ diceroll.py
@@ -0,0 +1,9 @@
+#Program by Pritheev
+#Dice Roller
+
+def MainMenu():
+    print("========================")
+    print("[1] Choose Dice Size")
+    print("[2] Roll Dice")
+    print("[0] Exit")
+    print("========================")
